b10ca77109321cd1c4b4a43bac166ba42a5073a7
akarelin <alex@karel.in>
2019-08-07 14:19:58 -0700

    Cleanup + added mqtt_prefix

--- mqtt_dt.py
+++ mqtt_dt.py
@@ -25,17 +25,18 @@ class MQTT_DT(hass.Hass):
     self._log(level, message, *args)
 
   def initialize(self):
-    if isinstance(self.args["prefix"], str):
-      self.prefix = self.args["prefix"]
+    if isinstance(self.args["entity_prefix"], str):
+      self.entity_prefix = self.args["entity_prefix"]
     else:
-      self.warn("Prefix is invalid {}", self.args["prefix"])
+      self.warn("Prefix is invalid {}", self.args["entity_prefix"])
       return
-    
+
+    if isinstance(self.args["mqtt_prefix"], str):
+      self.mqtt_prefix = self.args["mqtt_prefix"]
+    else:
+      self.mqtt_prefix = 'device'
+
     home = self.get_state("zone.home", attribute="all", namespace=self.args["tracker_namespace"])
-#    self.debug("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ DTs: {}", home)
-#    self.debug("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ DTs: {}", home['attributes'])
-#    return
-    
     self.payload = {}
     self.payload['latitude'] = home['attributes']['latitude']
     self.payload['longitude'] = home['attributes']['longitude']
@@ -43,8 +44,6 @@ class MQTT_DT(hass.Hass):
       self.payload['gps_accuracy'] = self.args['gps_accuracy']
     except:
       self.payload['gps_accuracy'] = home['attributes']['radius']
-#    self.debug("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ DTs: {}", self.payload)
-#    return
 
     self.trackers = {}
     for entity_id in self.get_state("group.all_devices", attribute="entity_id", namespace=self.args["tracker_namespace"]):
@@ -54,7 +53,6 @@ class MQTT_DT(hass.Hass):
         device=name.split('_')[1]
         self.trackers[entity_id] = {'name':name, 'user':user, 'device':device}
 
-#    self.set_namespace(self.args["tracker_namespace"])
     try:
       self.trigger = self.listen_state(self._trigger,
         [*self.trackers], new='home', duration=60, immediate=True,
@@ -64,6 +62,5 @@ class MQTT_DT(hass.Hass):
       return
 
   def _trigger(self, entity, attribute, old, new, kwargs):
-    topic = 'device/'+trackers[entity]['user']+'/'+trackers[entity]['device']
-    self.debug("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Topic: {} Payload: {}", topic, self.payload)
+    topic = self.mqtt_prefix+'/'+trackers[entity]['user']+'/'+trackers[entity]['device']
     self.call_service("mqtt/publish", topic=topic, payload=self.payload)
