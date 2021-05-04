ccfc08d5b7ba6c70ed3e30c7dfc5548e44c13980
Alex Karelin <alex@karelin.com>
2021-05-04 16:36:44 -0700

    Atlona-dev

--- atlona.py
+++ atlona.py
@@ -93,7 +93,7 @@ class Atlona(unibridge.MqttApp):
     self.send_command(cmd)
     self.refresh()
 
-# region Internals
+  # region Internals
   def get_input(self, i):
     hr = self._cmd(f"Display:Matrix:Get {i}")
     result = None
@@ -124,9 +124,9 @@ class Atlona(unibridge.MqttApp):
     self._end()
     return result
 
-# endregion
+  # endregion
 
-# region Telnet
+  # region Telnet
   def _start(self):
     self.tn = telnetlib.Telnet(self.address)
     a = self.tn.read_until(b"\n\n", 3)
@@ -155,4 +155,115 @@ class Atlona(unibridge.MqttApp):
     except:
       self.warn("Unable to parse {}", hr)
     return result
-# endregion
+  # endregion
+
+AVA_TN_TIMEOUT = 10
+class AVAccess(unibridge.MqttApp):
+  address = None
+  prefix = None
+  inputs = []
+  outputs = []
+  mp = [None, None, None, None]
+  tn = None
+
+  def initialize(self):
+    super().initialize()
+    self.address = self.args.get('address')
+    self.inputs = self.args.get('inputs')
+    self.outputs = self.args.get('outputs')
+    self.prefix = self.args.get('prefix')
+    self.debug(f"AVAccess matrix initialized\n    {self.address}  {self.prefix}\n    {self.inputs}\n    {self.outputs}\n")
+    self.refresh()
+    
+    self.mqtt.mqtt_subscribe(f"{self.prefix}/output/+/set")
+    self.mqtt.listen_event(self._mqtt_callback, "MQTT_MESSAGE", wildcard = f"{self.prefix}/output/#")
+
+  # region SETs
+  def _mqtt_callback(self, event_name, data, kwargs):
+    topic = data.get('topic')
+    payload = data.get('payload')
+    output = topic.split('/')[2]
+    device = payload
+    self.debug(f"Callback {output} {device}")
+    self.set_output(output, device)
+
+  def trigger(self, kwargs):
+    self.refresh()
+
+  def set_output(self, output, input):
+    try:
+      i_output = self.outputs.index(output) + 1
+      i_input = self.inputs.index(input) + 1
+    except:
+      self.error(f"Unknown IO {output} {input}")
+      return
+    cmd = f"SET SW hdmiin{i_input} hdmiout{out_index}"
+    self.debug(f"Sending command {cmd}")
+    result = self.send_command(cmd)
+    self.debug(f"Result {result}")
+    self.refresh()
+  # endregion
+
+  # region GETs
+  def refresh(self):
+    self.debug(f"Refresh Starting")
+    self.query()
+    self.debug(f"        Queried")
+    self.publish()
+    self.debug(f"        Published")
+
+  def query(self):
+    self.get_mp()
+
+  def publish(self):
+    mphr = self.mphr
+    self.mqtt.mqtt_publish(f"{self.prefix}/state", json.dumps(self.mphr))
+    for o_index, i_index in enumerate(mp):
+      self.mqtt.mqtt_publish(f"{self.prefix}/output/{o_index+1}/state", i_index)
+
+  @property
+  def mphr(self):
+    result = {}
+    for o_index, o in enumerate(outputs):
+      i = inputs[mp[o_index-1]]
+      result[o] = i
+    return result
+
+  def get_mp(self):
+    result = self.send_command("GET MP all")
+    self.debug(f"MPs: {result}")
+    for mp in result:
+      i_index = int(mp.split(" ")[1][-1])
+      o_index = int(mp.split(" ")[2][-1])
+      mp[o_index] = i_index
+    self.debug(f"    Parsed: {mp}")
+  # endregion
+
+  # region Telnet
+  def send_command(self, cmd):
+    self._start()
+    result = self._cmd(cmd)
+    self._end()
+    return result
+
+  def _start(self):
+    self.debug(f"Connecting to {self.address}:23 TO={AVA_TN_TIMEOUT}")
+    self.tn = telnetlib.Telnet(self.address, 23, AVA_TN_TIMEOUT)
+    self.debug(f"Connected to {self.address} handle={self.tn}")
+    welcome = self.tn.read_until(b"\n\r")
+    self.debug(f"Welcome {welcome}")
+
+  def _end(self):
+    self.tn.close()
+
+  def _cmd(self, cmd):
+    self.debug(f"CMD start {cmd}")
+    bcmd = cmd.encode('ascii')
+    self.tn.write(bcmd)
+    #raw = self.tn.read(b"\n\r", timeout)
+    self.debug(f"    written to buffer")
+    result = self.tn.read_all().splitlines()
+    #  response = raw.decode('ascii').split('\n#')[0]
+    self.debug(f"    Result {result}")
+    return result
+  # endregion
