f0a05d287bc001710a6d93005a5980ea6746ff46
Alex Karelin <alex@karelin.com>
2021-07-18 01:45:40 -0700

    Cleanup, phase 2

--- creekview.py
+++ creekview.py
@@ -327,7 +327,7 @@ class Indicator(u3.U3):
     topic = data.get('topic')
     payload = data.get('payload').lower()
     if self.transformer == 'Indicator2Switch': self.Indicator2Switch(topic, payload)
-  def Indicator2Switch(self, data):
+  def Indicator2Switch(self, topic, payload):
     entity = f"switch.{topic.replace('/','_')}"
     if payload in ['on','off']: self.hass.call_service(f"homeassistant/turn_{payload}", entity_id = entity)
 
